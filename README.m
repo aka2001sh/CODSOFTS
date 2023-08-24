5d05a1e2bb66ed67fad81d51cff29ea416e205ad
aka2001sh <142418265+aka2001sh@users.noreply.github.com>
2023-08-24 13:05:37 +0530

    Python Calculator

--- README.m
+++ README.m
@@ -1,3 +1,3 @@
-In this program, we ask the user to choose the desired operation. User-defined functions add(), subtract(), multiply() and divide() evaluate respective operations.
+In this program, User-defined functions add(), subtract(), multiply() and divide() evaluate respective operations.
 simple calculator with basic arithmetic operations and the user to input two numbers and an operation choice.Perform the calculation and display the result
 # CODSOFTS
